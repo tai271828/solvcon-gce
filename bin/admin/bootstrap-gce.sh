45ffcb692a1750969041ced4b87be2296ce2dc15
Yung-Yu Chen <yyc@solvcon.net>
2016-07-09 22:55:08 +0800

    Add bash account file appender.

--- bin/admin/bootstrap-gce.sh
+++ bin/admin/bootstrap-gce.sh
@@ -14,4 +14,8 @@ target=$HOME/opt/gce
 mkdir -p $target
 git clone $repo $target
 
+acctfile=~/.bash_acct
+enablestring='if [ -f ~/opt/gce/etc/gcerc ]; then source ~/opt/gce/etc/gcerc; fi'
+echo $enablestring >> $acctfile
+
 # vim: set et nobomb fenc=utf8 ft=sh ff=unix sw=2 ts=2:
