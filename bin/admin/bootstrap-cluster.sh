b84dcd12960181e82cf7e5463bf9137a2df7c092
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2016-09-19 17:08:12 +0800

    Ref issue 12 add script remark.
    This commit is according to the discussion of PR 11 to add the PR
    description to be the script remark. This commit is related to issue 10
    of project 1 as well. I also tweak the format and logic to show the
    usage description to make this script be more readable and maintainable.

--- bin/admin/bootstrap-cluster.sh
+++ bin/admin/bootstrap-cluster.sh
@@ -1,5 +1,82 @@
 #!/bin/bash
 
+usage_description() {
+  cat << EOM
+
+  This script is the basic component to bootstrap GCE instances to be a
+  very simple Beowulf cluster. Besides, this cluster could be used for
+  SOLVCON parallel computing with this bootstrap script. This commit is
+  the 1st step of the whole story of the issue #10. Wrappers to use this
+  bootstrap script will follow up in the later commits.
+
+  Test Case
+
+  1. gstart <master_node> to create a master node of our Beowulf cluster.
+
+  2. gstart <client_node_01> to create a client node of our Beowulf cluster.
+
+  3. gstart <client_node_02> to create another client node of our Beowulf
+  cluster.
+
+  4. gssh <master_node> to login <master_node>, and then issue
+  ./opt/gce/bin/admin/bootstrap-cluster.sh master to make this node be a NFS
+  server node, which will be a master node of our Beowulf cluster. Issue the
+  command hostname -i to get <master_node_IP>, which is the IP address we will
+  use when creating the other NFS client nodes.[1]
+
+  5. gssh <client_node_01> to login <client_node_01>, and then issue
+  ./opt/gce/bin/admin/bootstrap-cluster.sh client <master_node_IP> to make this
+  node be a NFS client node, which will be a client node of our Beowulf
+  cluster. <master_node_IP> could be known by the above step.[2]
+
+  6. go to ~/solvcon-remote/solvcon-src in <client_node_01>, issue source
+  activate to enable Conda environment, and then issue python setup.py
+  build_ext --inplace to build SOLVCON.
+
+  7. gssh <client_node_02> to login <client_node_02>, and then issue
+  ./opt/gce/bin/admin/bootstrap-cluster.sh client <master_node_IP> to make this
+  node be a NFS client node, which will be a client node of our Beowulf
+  cluster. <master_node_IP> could be known by the above step.[2]
+
+  8. go back to login <client_node_01>, and then export
+  SOLVCON_NODELIST=~/solvcon-remote/node_list to tell SOLVCON where to find the
+  available cluster nodes for parallel computing.
+
+  9. run nosetests ftests/parallel/test_remote.py -v to test the cluster.[3]
+  Please note this commit should be tested together with some customized setup
+  according your github repository and branch name. Please refer to this commit
+  5dc868b to create your test case branch as well.
+
+  Expected Result
+
+  [1] A default SOLVCON working directory, $HOME/solvcon-remote will be
+  created, and set up as a NFS folder. Besides, $HOME/.ssh will be set up as a
+  NFS folder as well to share the connection SSH key. Check the NFS setup is as
+  expected by issuing cat cat /etc/exports, and the output should has lines
+  like:
+
+  /home/tai271828/solvcon-remote *(rw,sync)
+  /home/tai271828/.ssh *(rw,sync)
+
+  [2] A default SOLVCON node list will be created (or appended IP if there is
+  one or more nodes already). Check ~/solvcon-remote/node_list to make sure the
+  IP of the node is appended in the node list.
+
+  [3] The test result should be
+
+  test_dsoln_and_parallel_output (test_remote.TestPresplitRemoteParallel) ... SKIP
+  test_dsoln (test_remote.TestRemoteParallel) ... ok
+  test_dsoln_cluster (test_remote.TestRemoteParallel) ... ok
+  test_runparallel (test_remote.TestTorqueParallel) ... SKIP
+
+  ----------------------------------------------------------------------
+  Ran 4 tests in 11.318s
+
+  OK (SKIP=2)
+
+EOM
+}
+
 build_master_node() {
   echo "building a master node..."
   # fetch SOLVCON source
@@ -72,8 +149,8 @@ build_nfs_server() {
   echo "building a NFS server...done!"
 }
 
-usage="Usage : $0 <node_type>"
-usage="$usage\n node_type: master, client, nfs_server"
+usage="Usage : $0 <node_type>\n"
+usage="${usage}\tnode_type: master, client, nfs_server\n"
 
 node_type=$1
 shift
@@ -97,12 +174,14 @@ case $key in
   shift
   ;;
   -h|--help)
-  help="1"
+  usage_description
+  printf "$usage"
+  exit
   ;;
   *)
   # unknown option
   echo "uknown option"
-  echo $usage
+  printf "$usage"
   exit
   ;;
 esac
@@ -122,14 +201,9 @@ case $node_type in
   *)
   # unknown node_type
   echo "unknown node_type"
-  echo $usage
+  printf "$usage"
   exit
   ;;
 esac
-
-if [ -n "$help" ]; then
-  echo $usage
-  exit
-fi
 
 # vim: set et nobomb fenc=utf8 ft=sh ff=unix sw=2 ts=2:
